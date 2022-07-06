3bbb4604a8fdc5fc7461993292025408a0e0fc39
absyinka <absyinka@gmail.com>
2022-07-06 16:04:47 -0700

    Fixed typo in result

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,7 +4,7 @@
 def fizzbuzz():
     for number in range(1, 101):
         if number % 3 == 0 and number % 5 == 0:
-            print("FizBuzz ", end="")
+            print("FizzBuzz ", end="")
         elif number % 3 == 0:
             print("Fizz ", end="")
         elif number % 5 == 0:
