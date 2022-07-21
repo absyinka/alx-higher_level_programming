7644433ceb8796e040338ccde9025d7dd3a72b27
absyinka <absyinka@gmail.com>
2022-07-21 05:27:31 -0700

    744 3-

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -10,6 +10,7 @@ def list_division(my_list_1, my_list_2, list_length):
             print("division by 0")
             result = 0
         except TypeError:
+            print("wrong type")
             result = 0
         except IndexError:
             print("out of range")
