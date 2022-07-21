e5d12e6c224c598fd6fceffe962ea48cefb0631a
absyinka <absyinka@gmail.com>
2022-07-21 04:02:56 -0700

    Whitespace removal

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,17 +2,12 @@
 
 
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        counter = 0
-        for i in range(0, x):
-            if isinstance(my_list[i], int):
-                print("{:d}".format(my_list[i]), end="")
-                counter += 1
-            else:
-                continue
-        counter += 1
-        print()
-        return counter
-    except (TypeError, ValueError):
-        print()
-        return counter
+    counter = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            counter += 1
+        except (TypeError, ValueError):
+            continue
+    print("")
+    return counter
