daa21c4fc18766b3d21826454c117e5109398c71
absyinka <absyinka@gmail.com>
2022-07-21 03:26:32 -0700

    Hot fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
+
+
 def safe_print_integer(value):
     try:
-        if isinstance(value, str):
-            return False
-        else:
-            print("{:d}".format(value))
-            return True
-    except Exception:
+        print("{:d}".format(value))
+        return True
+    except (TypeError, ValueError):
         return False
