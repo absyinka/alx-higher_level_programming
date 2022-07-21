b5b1ef440aa32f4c9d0427454909dcf63e0e2122
absyinka <absyinka@gmail.com>
2022-07-21 03:12:01 -0700

    Variable naming error fix

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
         counter = 0
         for i in range(0, x):
             print(f"{my_list[i]}", end="")
-            count += 1
+            counter += 1
         print()
         return counter
     except (TypeError, IndexError):
