085cadaf43d1f4dc5ab4d50678da994265c9f2c4
absyinka <absyinka@gmail.com>
2022-07-22 03:15:52 -0700

    Repushed

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -40,7 +40,7 @@ class Square:
             or not all(isinstance(num, int) for num in value)
             or not all(num >= 0 for num in value)
         ):
-            raise TypeError("position must be a tuple of 2 positive integer")
+            raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
