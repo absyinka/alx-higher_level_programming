43e66c563aad45df9deea666e33ea08dfc319ec9
absyinka <absyinka@gmail.com>
2022-07-22 01:18:31 -0700

    Documentation for task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+"""Square class with a private attribute instance"""
 
 
 class Square:
-	def __init__(self, size):
-		self.__size = size
+    """The size of the square initialization (constructor) method"""
+
+    def __init__(self, size):
+        self.__size = size
