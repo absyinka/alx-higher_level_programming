61af4b80838ea75459b8e8d4976012b2350275e9
absyinka <absyinka@gmail.com>
2022-07-22 01:29:01 -0700

    Documentation added to task 1

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""Square class with exception """
 
 
 class Square:
+    """Square class"""
+
     def __init__(self, size=0):
+        """Initial method with exception.
+        Excpetion raised if size is not an integer and is
+        less than 0.
+
+        Args:
+                size (int): The size of the new square with an optional value of 0
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
